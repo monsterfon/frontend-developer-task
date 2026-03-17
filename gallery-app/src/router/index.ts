8a6da8e9b6a804bd06c5da6d45cffe774b14a7a4
monsterfon <58121585+monsterfon@users.noreply.github.com>
2026-03-17 21:20:01 +0100

    add routes

--- gallery-app/src/router/index.ts
+++ gallery-app/src/router/index.ts
@@ -2,7 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'gallery',
+      component: () => import('@/views/GalleryView.vue'),
+    },
+    {
+      path: '/photo/:id',
+      name: 'detail',
+      component: () => import('@/views/DetailView.vue'),
+    },
+  ],
 })
 
 export default router
